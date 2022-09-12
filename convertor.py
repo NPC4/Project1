a60fc73c63dfb1df746dbdc4b2eb1656b9732008
NPC4 <aeghoneim@Outlook.com>
2022-09-12 10:37:06 +0300

    adding

---
+++ convertor.py
@@ -0,0 +1,10 @@
+import sys
+import os
+from PIL import Image
+
+#Grab pic & output location/name
+#Check if it exists, if not create new one
+#Loop through entire folder 1 by1
+#Convert
+#Save to new folder
+
